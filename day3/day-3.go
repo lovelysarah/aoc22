4d04ffd3050e8622b4089f671b1713dfc0df9a05
Sarah Robichaud <robichaudsarah@outlook.com>
2023-01-08 14:39:07 -0400

    ✨ Day 3, part 1

---
+++ day3/day-3.go
@@ -0,0 +1,62 @@
+package day3
+
+import (
+	"aoc22/utils"
+	"fmt"
+	"strings"
+)
+
+func Run() {
+	fmt.Println("-------- Day 3 ---------")
+	str, err := utils.ReadInput("/input/day-3.txt")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	lines := strings.Split(str, "\n")
+
+	alphabet := []rune{}
+	for i := 'a'; i <= 'z'; i++ {
+		alphabet = append(alphabet, i)
+	}
+	for i := 'A'; i <= 'Z'; i++ {
+		alphabet = append(alphabet, i)
+	}
+
+	var matches []rune
+	for i, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+
+		half := len(line) / 2
+
+		first, second := line[0:half], line[half:]
+
+		flag := false
+		for _, r1 := range first {
+			for _, r2 := range second {
+				if r1 == r2 && !flag {
+					matches = append(matches, r1)
+					flag = true
+				}
+			}
+		}
+
+		priorities := 0
+		for _, char := range matches {
+			for idx, c := range alphabet {
+				if char == c {
+					priorities += idx + 1
+				}
+			}
+		}
+
+		// Add +2 because of empty lines
+		if len(lines) == i+2 {
+			fmt.Printf("Priority sum: %v\n", priorities)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"aoc22/day1"
 	"aoc22/day2"
+	"aoc22/day3"
 )
 
 func main() {
 	day1.Run()
 	day2.Run()
+	day3.Run()
 }
